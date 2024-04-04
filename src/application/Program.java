1964800e4967df054c53e8ec9e1eeef654eb6d84
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2024-04-04 20:14:18 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,14 +2,19 @@ package application;
 
 import java.time.LocalDate;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
 		Department dep = new Department(1, "Books");
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.of(1972, 11, 5), 2500.0, dep);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 		
