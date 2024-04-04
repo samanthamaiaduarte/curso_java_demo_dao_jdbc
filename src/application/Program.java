ef7941f744c014fb8527744f0523ccbee23fbd89
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2024-04-04 20:06:44 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department dep = new Department(1, "Books");
-		System.out.println(dep);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", LocalDate.of(1972, 11, 5), 2500.0, dep);
+		
+		System.out.println(seller);
+		
 	}
 
 }
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -1,7 +1,6 @@
 package model.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 public class Department implements Serializable {
 
@@ -35,7 +34,10 @@ public class Department implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
 	@Override
@@ -47,7 +49,12 @@ public class Department implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Department other = (Department) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	@Override
